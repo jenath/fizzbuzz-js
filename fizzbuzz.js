ec98587487fef01b592abcf8972fb03a1353449b
Jenath <jenathkanagesan@gmail.com>
2022-09-28 14:42:06 +0100

    Add comments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,12 @@
 
 function fizzbuzz() {
+
+    /* Print numbers 1 - 100 */
+
     for (let i = 1; i <= 100; i++) {
         // console.log(i);
+
+    /* Print Fizz/Buzz/FizzBuzz depending on value*/
         
         if (i%3 === 0 & i%5 === 0) { 
             console.log("FizzBuzz");
