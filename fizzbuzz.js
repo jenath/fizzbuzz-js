8f7071612db70b44c7fe7a35759cd22c4cf6d4dd
Jenath <jenathkanagesan@gmail.com>
2022-09-28 15:58:48 +0100

    add bang

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,14 +6,18 @@ function fizzbuzz() {
     for (let i = 1; i <= 100; i++) {
         // console.log(i);
 
-    /* Print Fizz/Buzz/FizzBuzz depending on value*/
+    /* Print Fizz/Buzz/FizzBuzz/Bang depending on value*/
         
-        if (i%3 === 0 & i%5 === 0) { 
+        if (i%7 === 0 && i%3 === 0 || i%7 === 0 && i%5 === 0) {
+            console.log("")
+        } else if (i%3 === 0 & i%5 === 0) { 
             console.log("FizzBuzz");
         } else if (i%3) {
             console.log("Fizz");
         } else if (i%5 === 0) {
             console.log("Buzz");
+        } else if (i%7) {
+            console.log("Bang");
         }
 
     }
